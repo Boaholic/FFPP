2e1561eafa870553423f0ec447a51273fa2ef4ba
Skyler Schvaneveldt <boaholic@gmx.com>
2017-11-01 11:19:39 -0600

    Initial commit

---
+++ AppLayer/lobbyServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AppLayer
+{
+    
+    public class lobbyGame
+    {
+        public clientPlayer[] associatedPlayers;
+        public bool isActive;
+        public bool isOpen;
+        public lobbyGame()
+        {
+            associatedPlayers = null;
+            isActive = false;
+            isOpen = true;
+        }
+        public void playerJoinGame(clientPlayer newPlayer)
+        {
+            foreach (clientPlayer p in associatedPlayers)
+            {
+                if (newPlayer == p)
+                {
+                    return; //player is already in game
+                }
+            }
+            associatedPlayers.SetValue(newPlayer, associatedPlayers.GetUpperBound(1) + 1);
+        }
+    }
+    public class lobbyServer
+    {
+        public clientPlayer[] totalAssociatedPlayers;
+        public lobbyGame[] gamesInLobby;
+        public void newLobbyPlayer(clientPlayer newPlayer)
+        {
+            foreach (clientPlayer p in totalAssociatedPlayers)
+            {
+                if (newPlayer == p)
+                {
+                    return; //The player already exists
+                }
+            }
+            totalAssociatedPlayers.SetValue(newPlayer, totalAssociatedPlayers.GetUpperBound(1) + 1);
+        }
+
+        public lobbyServer()
+        {
+            totalAssociatedPlayers = null;
+            //Create two new instances of lobbygame
+            lobbyGame firstGame = new lobbyGame();
+            lobbyGame secondGame = new lobbyGame();
+            gamesInLobby.SetValue(firstGame, gamesInLobby.GetUpperBound(1) + 1);
+            gamesInLobby.SetValue(secondGame, gamesInLobby.GetUpperBound(1) + 1);
+        }
+    }
+}
---
+++ GUILayer/InitForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GUILayer
+{
+    public partial class clientPlayer : Form
+    {
+        bool valid = false;
+
+        public clientPlayer()
+        {
+           
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+        }
+
+        public bool ValidateIPv4(string ipString)
+        {
+            if (String.IsNullOrWhiteSpace(ipString))
+            {
+                return false;
+            }
+
+            string[] splitValues = ipString.Split('.');
+            if (splitValues.Length != 4)
+            {
+                return false;
+            }
+
+            byte tempForParsing;
+
+            return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if(!valid)
+            {
+                if (NameBox.Text == "")
+                {
+
+                }
+                if (IPBox.Text == "")
+                {
+
+                }
+                if (PortBox.Text == "")
+                {
+
+                }
+            }
+          
+
+        }
+    }
+}
