91ec9353c8f0a5fb567312205bbb99e9313dabcf
Joshua Wilding <joshua.wilding@aggiemail.usu.edu>
2017-11-09 12:42:14 -0700

    Modified the ReadWrite.EncodeMessage() methods to explicitly encode the JSON as UTF-8. This fixes the bug where the JSON could not be read. The unit test in this version runs correctly on my machine.

--- AppLayer/ReadWrite.cs
+++ AppLayer/ReadWrite.cs
@@ -1,6 +1,6 @@
 using System.IO;
 using System.Runtime.Serialization.Json;
-
+using System.Text;
 
 namespace AppLayer
 {
@@ -26,8 +26,11 @@ namespace AppLayer
             DataContractJsonSerializer messageWriter = new DataContractJsonSerializer(typeof(Message));
             log.Info("Message before encoding: " + targetMessage.ToString());
             messageWriter.WriteObject(writingStream, targetMessage);
-            log.Info("Message after encoding: " + writingStream.GetBuffer());
-            return writingStream.GetBuffer();
+            //http://www.advancesharp.com/blog/1086/convert-object-to-json-and-json-to-object-in-c
+            //https://connect.microsoft.com/VisualStudio/feedback/details/356750/datacontractjsonserializer-fails-with-non-ansi-characters
+            string messageJSON = Encoding.UTF8.GetString(writingStream.ToArray());
+            log.Info("Message after encoding: " + messageJSON);
+            return Encoding.UTF8.GetBytes(messageJSON);
         }
 
         public byte[] EncodeMessage(Message inputMessage)
@@ -37,8 +40,9 @@ namespace AppLayer
             DataContractJsonSerializer messageWriter = new DataContractJsonSerializer(typeof(Message));
             MemoryStream writingStream = new MemoryStream();
             messageWriter.WriteObject(writingStream, inputMessage);
-            log.Info("Message after encoding: " + writingStream.GetBuffer());
-            return writingStream.GetBuffer();
+            string messageJSON = Encoding.UTF8.GetString(writingStream.ToArray());
+            log.Info("Message after encoding: " + messageJSON);
+            return Encoding.UTF8.GetBytes(messageJSON);
         }
     }
 }
