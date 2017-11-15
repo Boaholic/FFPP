a8c5d7fccbebad85d7034dfef2f6e8f382d0e730
Skyler Schvaneveldt <boaholic@gmx.com>
2017-11-15 12:28:33 -0700

    Starting to set up the Lobby GUI

---
+++ GUILayer/Lobby.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GUILayer
+{
+    partial class Lobby
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChatWindow = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ChatWindow
+            // 
+            this.ChatWindow.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ChatWindow.Location = new System.Drawing.Point(28, 27);
+            this.ChatWindow.Multiline = true;
+            this.ChatWindow.Name = "ChatWindow";
+            this.ChatWindow.ReadOnly = true;
+            this.ChatWindow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatWindow.Size = new System.Drawing.Size(1019, 949);
+            this.ChatWindow.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 992);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(927, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(28, 991);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Chat!";
+            this.SendButton.UseVisualStyleBackColor = true;
+            // 
+            // Lobby
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1444, 1048);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ChatWindow);
+            this.Name = "Lobby";
+            this.Text = "Lobby";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ChatWindow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SendButton;
+    }
+}
